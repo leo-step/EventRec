a475de5914fa805c18150de709d4cd9fe3c97e91
Aaliyah Sayed <aaliyah.sayed@gmail.com>
2023-11-29 20:36:27 -0500

    create dataset

--- embed.py
+++ embed.py
@@ -15,48 +15,57 @@ def embed_people():
 def embed_events():
     with open("events.json", "r") as fp:
         events = json.load(fp)
-    vectors = []
-    for i, event_dict in enumerate(events):
-        if i % 10 == 1:
-            print(f"{i}/{len(events)}")
-        text = []
-        bad_keys = set(["DTSTAMP", "LAST-MODIFIED", "CREATED", "SEQUENCE", "CONTACT", "DTSTART", "DTEND", "UID", "URL"])
-        for key, value in event_dict.items():
-            if key in bad_keys:
-                continue
-            text.append(value)
-        text = '\n'.join(text)
-        vectors.append(get_embedding(text))
-    vectors = np.array(vectors)
-    np.save("event_vectors.npy", vectors)
+    for split in events:
+        vectors = []
+        for i, event_dict in enumerate(events[split]):
+            if i % 10 == 1:
+                print(f"{i}/{len(events[split])}")
+            text = []
+            bad_keys = set(["DTSTAMP", "LAST-MODIFIED", "CREATED", "SEQUENCE", "CONTACT", "DTSTART", "DTEND", "UID", "URL"])
+            for key, value in event_dict.items():
+                if key in bad_keys:
+                    continue
+                text.append(value)
+            text = '\n'.join(text)
+            vectors.append(get_embedding(text))
+        vectors = np.array(vectors)
+        np.save(f"event_{split}_vectors.npy", vectors)
 
 def create_dataset():
     # index of person: [event indexes]
     people_vecs = np.load("people_vectors.npy")
-    event_vecs = np.load("event_vectors.npy")
-    dataset = dict()
 
-    for i, person_vec in enumerate(people_vecs):
-        event_indexes = top_k_similar(person_vec, 10, event_vecs)
-        dataset[i] = event_indexes.tolist()
+    dataset = {
+        "train": {},
+        "val": {},
+        "test": {}
+    }
+
+    for split in dataset:
+        event_vecs = np.load(f"event_{split}_vectors.npy")
+        k = 30 if split == "train" else 10
+        for i, person_vec in enumerate(people_vecs):
+            event_indexes = top_k_similar(person_vec, k, event_vecs)
+            dataset[split][i] = event_indexes.tolist()
     
     with open("dataset.json", "w") as fp:
         json.dump(dataset, fp)
 
 if __name__ == "__main__":
-    with open("people.json", "r") as fp:
-        people = json.load(fp)
-    with open("events.json", "r") as fp:
-        events = json.load(fp)
-    with open("dataset.json", "r") as fp:
-        dataset = json.load(fp)
-
-    print(people[20])
-    print()
-
-    recommended = get_results(events, dataset["20"])
-
-    for event in recommended:
-        print(event["SUMMARY;ENCODING=QUOTED-PRINTABLE"])
-        print(event["DESCRIPTION"])
-        print()
+    create_dataset()
+    # with open("people.json", "r") as fp:
+    #     people = json.load(fp)
+    # with open("events.json", "r") as fp:
+    #     events = json.load(fp)
+    # with open("dataset.json", "r") as fp:
+    #     dataset = json.load(fp)
+    #
+    # print(people[20])
+    # print()
+    #
+    # recommended = get_results(events, dataset["20"])
+    #
+    # for event in recommended:
+    #     print(event["SUMMARY;ENCODING=QUOTED-PRINTABLE"])
+    #     print(event["DESCRIPTION"])
+    #     print()
--- parse_ical.py
+++ parse_ical.py
@@ -6,9 +6,16 @@ def extract_events(file_name):
         text = ''.join(fp.readlines())
     pattern = re.compile(r'BEGIN:VEVENT\n(.*?)END:VEVENT', re.DOTALL)
     matches = re.findall(pattern, text)
-    dt1 = datetime(2023, 11, 28, 8, 15, 17)
-    dt2 = datetime(2023, 12, 31, 9, 30, 45)
-    events = []
+    dt1 = datetime(2023, 8, 1, 8, 15, 17)
+    dt2 = datetime(2023, 10, 31, 9, 30, 45)
+    dt3 = datetime(2023, 11, 15, 9, 30, 45)
+    dt4 = datetime(2024, 1, 22, 9, 30, 45)
+    events_train = []
+    events_val = []
+    events_test = []
+    # count_train = 0
+    # count_val = 0
+    # count_test = 0
     for match in matches:
         event_dict = {}
         lines = match.split('\n')
@@ -21,14 +28,26 @@ def extract_events(file_name):
             event_dict[key] = value
 
         start = datetime.strptime(event_dict["DTSTART"], "%Y%m%dT%H%M%SZ")
+
         if dt1 <= start <= dt2:
-            events.append(event_dict)
-    return events
+            events_train.append(event_dict)
+        elif dt2 < start <= dt3:
+            events_val.append(event_dict)
+        elif dt3 < start <= dt4:
+            events_test.append(event_dict)
+
+    return events_train, events_val, events_test
 
 if __name__ == "__main__":
     import json
 
-    events = extract_events("ical_princeton.ics")
+    events_train, events_val, events_test = extract_events("ical_princeton.ics")
+
+    events = {
+        "train": events_train,
+        "val": events_val,
+        "test": events_test
+    }
 
     with open("events.json", "w") as fp:
         json.dump(events, fp)
